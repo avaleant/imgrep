5d8a50224551c4e800908de9772d9c0a7fad6b9d
eyewheel <185627573+eyewheel@users.noreply.github.com>
2024-10-19 13:21:50 -0600

    Update search functionality to, in very primitive fashion, take both transcriptions into account. Also move to using fuzz.partial_ratio, which more appropriately examines substrings rather than the whole string for similarity.

--- search.py
+++ search.py
@@ -4,10 +4,16 @@ import sys
 
 db = TinyDB('memes.json')
 
-# fuzz.ratio based on a key in a value set
+# Extremely primitive way to fuzz.partial_ratio based on two keys in a value set
+# taking their average.
 def ratio_hk(e1, e2, processor=None, score_cutoff=None):
-    return fuzz.ratio(e1, e2['transcription'], 
+    to_llm = fuzz.partial_ratio(e1, e2['llm_transcription'], 
                       processor=processor, score_cutoff=score_cutoff)
+    # print(f"Score for {e2['llm_transcription']} is {to_llm}")
+    to_ocr = fuzz.partial_ratio(e1, e2['ocr_transcription'], 
+                      processor=processor, score_cutoff=score_cutoff)
+    # print(f"Score for {e2['ocr_transcription']} is {to_ocr}")
+    return (to_llm + to_ocr) / 2
 
 def search(query, limit=5):
     return process.extract(query, db.all(), limit=limit, scorer=ratio_hk)
@@ -16,4 +22,6 @@ if __name__ == "__main__":
     query = ' '.join(sys.argv[1:len(sys.argv)])
     result = search(query, limit=1)
     for possible in result:
-        print(f"{possible[0]['file']}: {possible[0]['transcription']}")
+        print(f"{possible[0]['file']}:")
+        print(f"{possible[0]['llm_transcription']}")
+        print(f"{possible[0]['ocr_transcription']}")
